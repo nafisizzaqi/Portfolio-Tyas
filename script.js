93d71f14adee06c87860b191e864f0ea9dccab89
Nafis <znafisiz@gmail.com>
2024-11-14 11:57:12 +0700

    portfolio milik tyas

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var typingEffect = new Typed("#typings", {
+    strings: ["3D Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    startDelay: 1000,
+    backDelay: 1500
+});
